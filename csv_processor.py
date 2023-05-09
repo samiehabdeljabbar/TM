3225a5700110588dcc8f71f8f9c380ec6bbb40f9
Samih Abdeljabbar <abdels6375@gmail.com>
2023-05-09 00:00:04 -0500

    trying to run a function to run this code to see if it actually works

---
+++ csv_processor.py
@@ -0,0 +1,41 @@
+import os
+import time
+import pandas as pd
+
+folder_path = "/Users/samihabdeljabbar/TM/example_folder"
+
+process_csv(folder_path):
+    while True:
+        # Get the list of CSV files in the folder
+        csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
+
+        # Check if there is at least one CSV file in the folder
+        if len(csv_files) > 0:
+            # Get the first CSV file in the list (assuming there is only one CSV file in the folder)
+            csv_file = csv_files[0]
+
+            # Read the CSV file into a DataFrame
+            df = pd.read_csv(os.path.join(folder_path, csv_file))
+
+            # Create separate DataFrames for withdrawals, deposits, and checks
+            withdrawals = df[df['Transaction Type'] == 'Withdrawal']
+            deposits = df[df['Transaction Type'] == 'Deposit']
+            checks = df[df['Transaction Type'] == 'Check']
+
+            # Create a Pandas Excel writer using xlsxwriter as the engine
+            writer = pd.ExcelWriter('transactions.xlsx', engine='xlsxwriter')
+
+            # Write each DataFrame to a different worksheet in the same workbook
+            withdrawals.to_excel(writer, sheet_name='Withdrawals', index=False)
+            deposits.to_excel(writer, sheet_name='Deposits', index=False)
+            checks.to_excel(writer, sheet_name='Checks', index=False)
+
+            # Save the workbook
+            writer.save()
+
+            # Wait for 5 seconds before checking the folder again
+            time.sleep(5)
+        else:
+            # Wait for 5 seconds before checking the folder again
+            time.sleep(5)
+
